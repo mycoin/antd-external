b28a859e12bf049e1ed9d3cce8ed730f4a5865a6
mycoin <1845440+mycoin@users.noreply.github.com>
2023-11-20 16:45:22 +0800

    Dist

--- src/supports/showLoading.tsx
+++ src/supports/showLoading.tsx
@@ -1,14 +1,16 @@
 import { ReactNode } from 'react'
 import { utils } from 'rigel-base'
+import { ArgsProps } from 'antd/lib/message'
 import showMessage, { callMessageOpen } from './showMessage'
 
-export default (content: ReactNode) => {
+export default (content: ReactNode, params?: ArgsProps) => {
   const key = utils.getGuid(11)
   const hide = callMessageOpen({
     type: 'loading',
     key,
     content: content || '正在加载中',
     onClick: null,
+    ...params,
   })
 
   return (success?: boolean, contentNode?: ReactNode) => {
