35ccc71f495c477a54d044441521df90235d95a0
mycoin <1845440+mycoin@users.noreply.github.com>
2024-03-14 13:39:08 +0800

    feat: [auto-commit]

--- src/supports/val.ts
+++ src/supports/val.ts
@@ -1,6 +1,14 @@
+const getNativeElement = (eventOrValue: any) => {
+  const { nativeEvent, target } = eventOrValue || {}
+  if (nativeEvent && nativeEvent.target) {
+    return nativeEvent.target
+  }
+  return target
+}
+
 // 判断对象是否有值
 export default <T = any>(eventOrValue: any): T => {
-  const { target } = eventOrValue || {}
+  const target = getNativeElement(eventOrValue)
 
   if (target && target.tagName) {
     const { type, checked, files, value } = target
